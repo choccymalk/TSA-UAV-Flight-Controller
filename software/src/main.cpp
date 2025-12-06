02ccbfe407b93837a7561d662d15b5d06234a6cf
Bentley Smith <79180114+choccymalk@users.noreply.github.com>
2025-12-06 15:36:42 -0500

    fix bugs

--- software/src/main.cpp
+++ software/src/main.cpp
@@ -16,16 +16,23 @@ ceSerial com("/dev/ttyACM0",115200,8,'N',1);
 httplib::Server svr;
 
 std::string readSerialData() {
-    char initialChar = com.ReadChar(true);
+    bool readSuccess = true; // assume serial will read successfully, com.readChar will update this if it wasn't
+    char initialChar = com.ReadChar(readSuccess);
+    if(!readSuccess) {
+        return "Error reading from serial port";
+    }
     if (initialChar != 'B') {
-        return "";  // Not a valid message
+        return "Not a valid message";  // Not a valid message
     }
 
     std::vector<char> buffer(800);  // Use vector for safe memory management
     int i = 0;
 
     while (true) {
-        char nextChar = com.ReadChar(true);
+        char nextChar = com.ReadChar(readSuccess);
+        if(!readSuccess) {
+            return "Error reading from serial port";
+        }
         if (nextChar == 'E') {
             break;  // End of message
         }
@@ -43,7 +50,7 @@ std::string readSerialData() {
 std::string parseMessage(const std::string& data) {
     size_t pos = 0;
     size_t sizeOfCurrentBlock = 0;  // iterator for current block size, each block is 8 bytes
-    if (data[0] != 'B') return;
+    if (data[0] != 'B') return "Invalid start character";
 
     pos = 1;  // Skip 'B'
 
